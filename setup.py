239e778010d7ce42ca7cb606ce335fc515340fc8
tomioke <tomiirvan@gmail.com>
2022-01-24 12:56:40 +0700

    initial commit by tomi

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-mysql',
+    version='1.0.0',
+    author='Tomi Irvan Oktavianto',
+    author_email='tomiirvan@gmail.com',
+    packages=find_packages(),
+)
