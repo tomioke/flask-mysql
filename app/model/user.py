ed0ac365ce74d6f7673f362dd652330b60f04698
tomioke <tomiirvan@gmail.com>
2022-01-26 08:03:04 +0700

    update

---
+++ app/model/user.py
@@ -0,0 +1,12 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
