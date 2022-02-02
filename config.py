7b536e3a1a92614676d7a1c94261373eb93f71b6
tomioke <tomiirvan@gmail.com>
2022-02-03 06:54:54 +0700

    Update config

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config(object):
 
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
     # SQLALCHEMY_TRACK_NOTIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
