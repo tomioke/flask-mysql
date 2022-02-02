b9a43315233b59c32bbcf3156f59bb6d5a594073
tomioke <tomiirvan@gmail.com>
2022-02-02 11:02:43 +0700

    New update

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import sys
+import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app) # Membaca app
 migrate = Migrate(app, db) # hubungkan app dengan db
+
+# Menampilkan pesan error pada heroku
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 from app.model import user, guru, siswa
 from app import routes
