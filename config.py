bf9e777eda56765324d3c76c5c6f8d86332270b8
tomioke <tomiirvan@gmail.com>
2022-02-02 10:59:50 +0700

    Updates

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    HOST = str(os.environ.get("CLEARDB_DATABASE_URL")); # Setting dari heroku CLEARDB_DATABASE_URL
+    HOST = str(os.environ.get("DATABASE_URL")); # Setting dari heroku CLEARDB_DATABASE_URL
     DATABASE = str(os.environ.get("DB_DATABASE"))
     USERNAME = str(os.environ.get("DB_USERNAME"))
     PASSWORD = str(os.environ.get("DB_PASSWORD"))
 
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
